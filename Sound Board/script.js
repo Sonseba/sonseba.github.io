062c7ea307894c9a2104c920cbc82defe708910e
Sonseba <67759626+Sonseba@users.noreply.github.com>
2024-07-10 21:43:27 +0100

    New Projects

---
+++ Sound Board/script.js
@@ -0,0 +1,24 @@
+const sounds = ['applause','boo','gasp','tada','victory','wrong']
+currentSound = null
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+
+    btn.innerText = sound;
+
+    btn.addEventListener('click', () => {
+        if(currentSound){
+            currentSound.pause();
+            currentSound.currentTime = 0;
+
+        }
+        currentSound = document.getElementById(sound);
+        currentSound.play();
+    })
+
+    document.getElementById('buttons').appendChild(btn);
+
+}
+
+)
