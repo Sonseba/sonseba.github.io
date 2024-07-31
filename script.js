f87ae01633f22dc5344b619c51e3d3931075213e
Sonseba <67759626+Sonseba@users.noreply.github.com>
2024-07-31 21:39:36 +0100

    Remove alphabetical ordering

--- script.js
+++
@@ -1,5 +0,0 @@
-let div = document.getElementById('container');
-let buttons = Array.from(div.children);
-buttons.sort((a, b) => a.innerText.localeCompare(b.innerText))
-div.innerHTML = '';
-buttons.forEach(b => div.appendChild(b));
